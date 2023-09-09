8a4c03f1d8b4bdc07503e118291c04f57126ebdb
Cvlmtt <97099847+Cvlmtt@users.noreply.github.com>
2023-09-09 12:04:28 +0200

    Add Gaussian NB, Bernoulli NB and Complement NB

--- agent.py
+++ agent.py
@@ -1,5 +1,5 @@
 import random
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import BernoulliNB, ComplementNB, GaussianNB
 from sklearn.model_selection import cross_validate, RepeatedKFold
 from sklearn.metrics import (
     precision_score,
@@ -9,13 +9,23 @@ from sklearn.metrics import (
     matthews_corrcoef,
     
 )
+import statistics
+
 
 class Agent:
     model = None
 
 
-    def __init__(self) -> None:
-        self.model = GaussianNB()
+    def __init__(self, model_type="Complement") -> None:
+        if(model_type == "Complement"):
+            print("Complement NB implemented")
+            self.model = ComplementNB()
+        elif(model_type == "Gaussian"):
+            print("Gaussian NB implemented")
+            self.model = GaussianNB()
+        elif(model_type == "Bernoulli"):
+            print("Bernoulli NB implemented")
+            self.model = BernoulliNB()
 
 
     def fit(self, X_train, y_train):
@@ -36,5 +46,10 @@ class Agent:
     def cross_validation(self, X_train, y_train):
         rfk = RepeatedKFold(n_splits=10, n_repeats=4, random_state=random.randint(0,256))
         tests=list(["accuracy", "precision", "recall"])
-        cv_scores = cross_validate(self.model, X_train, y_train, cv=rfk, n_jobs=3, verbose=5, scoring=tests)
-        return cv_scores
+        cv_score = cross_validate(self.model, X_train, y_train, cv=rfk, n_jobs=3, verbose=5, scoring=tests)
+        fit_time_mean = statistics.mean(cv_score['fit_time'])
+        score_time_mean = statistics.mean(cv_score['score_time'])
+        accuracy_mean = statistics.mean(cv_score['test_accuracy'])
+        precision_mean = statistics.mean(cv_score['test_precision'])
+        recall_mean = statistics.mean(cv_score['test_recall'])
+        return fit_time_mean, score_time_mean, accuracy_mean, precision_mean, recall_mean
--- test.py
+++ test.py
@@ -39,16 +39,20 @@ print(separetor)
 ham_counter, spam_counter = coun_spam_ham(dipendent_variables_test_set)
 print(f"Test entrys -> Ham emails: {ham_counter}, Spam emails: {spam_counter}\n")
 
+
+#GaussianNB
 #Traingins
-agent = Agent()
+print("Gaussian Naive Bayes agent:")
+print(separetor)
+gaussian_agent = Agent(model_type="Gaussian")
 print(separetor)
 print("Agent training...\n")
-agent.fit(indipendent_variables_training_set, dipendent_variables_training_set)
+gaussian_agent.fit(indipendent_variables_training_set, dipendent_variables_training_set)
 
 #Testing
 print(separetor)
 print("Agent predicting...\n")
-dipendent_variables_prediction = agent.predict(indipendent_variables_testing_set) 
+dipendent_variables_prediction = gaussian_agent.predict(indipendent_variables_testing_set) 
 
 
 #Evalutating
@@ -56,13 +60,92 @@ print(separetor)
 print("Agent evaluating...")
 
 lables = [0, 1]
-accurancy, precision, recall, cm , mcc= agent.valuation(dipendent_variables_test_set, dipendent_variables_prediction, labels=lables)
+g_accurancy, g_precision, g_recall, g_cm , g_mcc = gaussian_agent.valuation(dipendent_variables_test_set, dipendent_variables_prediction, labels=lables)
 
-print(f"Accurancy:{'%.2f'%(accurancy*100)}%\nPrecision:{'%.2f'%(precision*100)}%\nRecall:{'%.2f'%(recall*100)}%\nMCC: {'%.2f'%(mcc*100)}%")
+print(f"Gaussian Accurancy:{'%.2f'%(g_accurancy*100)}%\nGaussian Precision:{'%.2f'%(g_precision*100)}%\nGaussian Recall:{'%.2f'%(g_recall*100)}%\nGaussian MCC: {'%.2f'%(g_mcc*100)}%")
+print(separetor)
 
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=lables)
+disp = ConfusionMatrixDisplay(confusion_matrix=g_cm, display_labels=lables)
 disp.plot()
 
-cv_score = agent.cross_validation(X_train=indipendent_variables_training_set, y_train=dipendent_variables_training_set)
-print(cv_score)
+
+g_fit_time_mean, g_score_time_mean, g_accuracy_mean, g_precision_mean, g_recall_mean = gaussian_agent.cross_validation(X_train=indipendent_variables_training_set, y_train=dipendent_variables_training_set)
+print(separetor)
+print("Cross validatio means:\n")
+print(f"Gaussian Fit time mean:{g_fit_time_mean}\nGaussian Score time mean:{g_score_time_mean}\nGaussian Accuracy test mean: {g_accuracy_mean}\nGaussian Precision test mean: {g_precision_mean}\nGaussian Recall test mean:{g_recall_mean}")
+print(separetor)
+print(g_cm)
+plt.show()
+
+
+#BernoulliNB
+#Traingins
+print("Bernoulli Naive Bayes agent:")
+bernoulli_agent = Agent(model_type="Bernoulli")
+print(separetor)
+print("Agent training...\n")
+bernoulli_agent.fit(indipendent_variables_training_set, dipendent_variables_training_set)
+
+#Testing
+print(separetor)
+print("Agent predicting...\n")
+dipendent_variables_prediction = bernoulli_agent.predict(indipendent_variables_testing_set) 
+
+
+#Evalutating
+print(separetor)
+print("Agent evaluating...")
+
+lables = [0, 1]
+b_accurancy, b_precision, b_recall, b_cm , b_mcc = bernoulli_agent.valuation(dipendent_variables_test_set, dipendent_variables_prediction, labels=lables)
+
+print(f"Bernoulli Accurancy:{'%.2f'%(b_accurancy*100)}%\nBernoulli Precision:{'%.2f'%(b_precision*100)}%\nBernoulli Recall:{'%.2f'%(b_recall*100)}%\nBernoulli MCC: {'%.2f'%(b_mcc*100)}%")
+print(separetor)
+
+disp = ConfusionMatrixDisplay(confusion_matrix=b_cm, display_labels=lables)
+disp.plot()
+
+
+b_fit_time_mean, b_score_time_mean, b_accuracy_mean, b_precision_mean, b_recall_mean = bernoulli_agent.cross_validation(X_train=indipendent_variables_training_set, y_train=dipendent_variables_training_set)
+print(separetor)
+print("Cross validatio means:\n")
+print(f"Bernoulli Fit time mean:{b_fit_time_mean}\nBernoulli Score time mean:{b_score_time_mean}\nBernoulli Accuracy test mean: {b_accuracy_mean}\nBernoulli Precision test mean: {b_precision_mean}\nBernoulli Recall test mean:{b_recall_mean}")
+print(separetor)
+print(b_cm)
+plt.show()
+
+#ComplementNB
+#Traingins
+print("Complement Naive Bayes agent:")
+complement_agent = Agent(model_type="Complement")
+print(separetor)
+print("Agent training...\n")
+complement_agent.fit(indipendent_variables_training_set, dipendent_variables_training_set)
+
+#Testing
+print(separetor)
+print("Agent predicting...\n")
+dipendent_variables_prediction = complement_agent.predict(indipendent_variables_testing_set) 
+
+
+#Evalutating
+print(separetor)
+print("Agent evaluating...")
+
+lables = [0, 1]
+c_accurancy, c_precision, c_recall, c_cm , c_mcc= complement_agent.valuation(dipendent_variables_test_set, dipendent_variables_prediction, labels=lables)
+
+print(f"Complement Accurancy:{'%.2f'%(c_accurancy*100)}%\nComplement Precision:{'%.2f'%(c_precision*100)}%\nComplement Recall:{'%.2f'%(c_recall*100)}%\nComplement MCC: {'%.2f'%(c_mcc*100)}%")
+print(separetor)
+
+disp = ConfusionMatrixDisplay(confusion_matrix=c_cm, display_labels=lables)
+disp.plot()
+
+
+c_fit_time_mean, c_score_time_mean, c_accuracy_mean, c_precision_mean, c_recall_mean = complement_agent.cross_validation(X_train=indipendent_variables_training_set, y_train=dipendent_variables_training_set)
+print(separetor)
+print("Cross validatio means:\n")
+print(f"Complement Fit time mean:{c_fit_time_mean}\nComplement Score time mean:{c_score_time_mean}\nComplement Accuracy test mean: {c_accuracy_mean}\nComplement Precision test mean: {c_precision_mean}\nComplement Recall test mean:{c_recall_mean}")
+print(separetor)
+print(c_cm)
 plt.show()
